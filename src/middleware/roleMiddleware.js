d537a25f344e970b6d8532c576f3348a8e3c5705
nktoan1905 <nktoan1905@gmail.com>
2023-12-21 22:15:50 +0700

    verify token

---
+++ src/middleware/roleMiddleware.js
@@ -0,0 +1,33 @@
+import HttpStatusCode from '../helpers/httpStatusCode';
+import tokenMiddleware from './tokenMiddleware';
+
+const roleMiddleware = {
+	verifyAdmin: async (req, res, next) => {
+		await tokenMiddleware.verifyToken(req, res, () => {
+			if (req.user.roleId === UserRole.ADMIN) {
+				next();
+			} else {
+				res.status(403).json({ message: "You don't have permission to access!" });
+			}
+		});
+	},
+	verifyEmployees: async (req, res, next) => {
+		await tokenMiddleware.verifyToken(req, res, () => {
+			if (req.user.roleId === UserRole.EMPLOYEE) {
+				next();
+			} else {
+				res.status(HttpStatusCode.FORBIDDEN).json({ message: "You don't have permission to access!" });
+			}
+		});
+	},
+	verifyAdminOrEmployee: async (req, res, next) => {
+		await tokenMiddleware.verifyToken(req, res, () => {
+			if (req.user.roleId === UserRole.EMPLOYEE || req.user.roleId === UserRole.ADMIN) {
+				next();
+			} else {
+				res.status(HttpStatusCode.FORBIDDEN).json({ message: "You don't have permission to access!" });
+			}
+		});
+	},
+};
+export default roleMiddleware;
---
+++ src/middleware/tokenMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import HttpStatusCode from '../helpers/httpStatusCode';
+import db from '../models';
+
+const tokenMiddleware = {
+	verifyToken: async (req, res, next) => {
+		const token = req.headers.token;
+		if (token) {
+			const accessToken = token.split(' ')[1];
+			jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, async (err, user) => {
+				if (err) {
+					return res.status(HttpStatusCode.FORBIDDEN).json({ message: 'Token is not valid' });
+				}
+				const userDB = await db.User.findOne({ where: { id: user.id } });
+				if (userDB.status === true) {
+					req.user = user;
+					next();
+				} else {
+					return res
+						.status(HttpStatusCode.NOT_ACCEPTABLE)
+						.json({ message: 'This account has been delete. Please contact with us' });
+				}
+			});
+		} else {
+			return res.status(HttpStatusCode.UNAUTHORIIZED).json({ message: "You're not authenticated" });
+		}
+	},
+};
+export default tokenMiddleware;
